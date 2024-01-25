68943d18b43912062fdbb54eca278ca9f2d40187
Mihai Todor <todormihai@gmail.com>
2024-01-25 20:29:19 +0000

    Refactor utilities data fetching

---
+++ fetch_utilities_data.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -eu
+
+. ./.env
+
+if [ -d ./data ]; then
+    echo "files already fetched."
+    exit 0
+fi
+
+mkdir -p ./data/refseq
+(
+    cd ./data/refseq
+
+    echo "Downloading refseq files..."
+
+    curl -sLO https://github.com/FHIR/genomics-operations/releases/download/${UTILITIES_DATA_VERSION}/GRCh37_refseq.tar.gz
+    curl -sLO https://github.com/FHIR/genomics-operations/releases/download/${UTILITIES_DATA_VERSION}/GRCh38_refseq.tar.gz
+    curl -sLO https://github.com/FHIR/genomics-operations/releases/download/${UTILITIES_DATA_VERSION}/rna_refseq.tar.gz
+
+    echo "Extracting refseq files..."
+
+    tar -xzf GRCh37_refseq.tar.gz
+    tar -xzf GRCh38_refseq.tar.gz
+    tar -xzf rna_refseq.tar.gz
+
+    echo "Finished extracting refseq files."
+)
+
+mkdir -p ./data/pyard
+(
+    cd ./data/pyard
+
+    echo "Downloading pyard database..."
+
+    curl -sLO https://github.com/FHIR/genomics-operations/releases/download/${UTILITIES_DATA_VERSION}/pyard.sqlite3.tar.gz
+
+    echo "Extracting pyard database..."
+
+    tar -xzf pyard.sqlite3.tar.gz
+
+    echo "Finished extracting pyard database."
+)
